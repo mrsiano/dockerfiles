e15768ab00aa34ebaeb20ae186f4f48d725ea75a
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2017-09-01 18:59:42 -0400

    Refactor dockerfile to avoid systemd

--- collectd/script.sh
+++ collectd/script.sh
@@ -4,5 +4,9 @@ if [ -d /proc_host ]; then
   umount /proc
   mount -o bind /proc_host /proc
 fi
-systemctl restart collectd
-while true; do sleep 1; done
+
+sed -i "/graphite_host/c \ \ \ \ Host ${graphite_host}" /etc/collectd.conf
+sed -i "/graphite_prefix/c \ \ \ \ Host ${graphite_prefix}" /etc/collectd.conf
+
+# run collectd
+/usr/sbin/collectd -f
--- pbench-agent/mount.sh
+++ pbench-agent/mount.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /var/run/pbench/keys
 . /etc/profile.d/pbench-agent.sh
 mkdir -p /var/lib/pbench-agent/tools-default
 mount -o bind /proc_host /proc
+while true; do sleep 1; done
