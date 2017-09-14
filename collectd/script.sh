bb9cb7821cdb63d0c3ee5cfc5e9c31e759612570
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2017-09-14 15:32:30 -0400

    Expose 9090 port for http tests

--- collectd/script.sh
+++ collectd/script.sh
@@ -5,8 +5,8 @@ if [ -d /proc_host ]; then
   mount -o bind /proc_host /proc
 fi
 
-sed -i "/graphite_host/c \ \ \ \ Host ${graphite_host}" /etc/collectd.conf
-sed -i "/graphite_prefix/c \ \ \ \ Host ${graphite_prefix}" /etc/collectd.conf
+#sed -i "/graphite_host/c \ \ \ \ Host ${graphite_host}" /etc/collectd.conf
+#sed -i "/graphite_prefix/c \ \ \ \ Host ${graphite_prefix}" /etc/collectd.conf
 
 # run collectd
 /usr/sbin/collectd -f
--- pbench-controller/script.sh
+++ pbench-controller/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 . /etc/profile.d/pbench-agent.sh
 mkdir -p /var/lib/pbench-agent/tools-default
 cp /root/.ssh/id_rsa /opt/pbench-agent/id_rsa
@@ -7,11 +6,29 @@ sed -i "/^pbench_results_redirector/c pbench_results_redirector = ${pbench_serve
 sed -i "/^pbench_web_server/c pbench_web_server = ${pbench_server}"  /opt/pbench-agent/config/pbench-agent.cfg
 pbench-clear-tools
 ansible-playbook -i /root/inventory /root/pbench/contrib/ansible/openshift/pbench_register.yml
+if [[ "$?" == 0 ]]; then
+        echo "-----------------------------------------------------------"
+        echo "PBENCH TOOLS REGISTERED SUCCESSFULLY"
+        echo "-----------------------------------------------------------"
+else
+        echo "-----------------------------------------------------------"
+        echo "PBENCH REGISTRATION FAILED"
+        echo "-----------------------------------------------------------"
+fi
 if [[ "${clear_results}" == "true" ]] || [[ "${clear_results}" == "True" ]]; then
        pbench-clear-results
 fi       
 ${benchmark}
-if [[ "${move_results}" == "true" ]] || [[ "${move_results}" == "True" ]]; then  
-	pbench-move-results
+benchmark_status=$?
+if [[ "${move_results}" == "true" ]] || [[ "${move_results}" == "True" ]]; then
+        pbench-move-results
 fi
-while true; do sleep 1; done
+if [[ "$?" == 0 ]]; then
+	echo "-----------------------------------------------------------"
+	echo "OCP SCALE TEST COMPLETED"
+	echo "-----------------------------------------------------------"
+else
+	echo "-----------------------------------------------------------"
+	echo "OCP SCALE TEST FAILED"
+	echo "-----------------------------------------------------------"
+fi
