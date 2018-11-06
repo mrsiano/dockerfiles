081af62a4fb56e45ac20fae9d6f58a12834843ea
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2018-11-06 12:59:19 -0500

    Controller changes to deploy it as a kubernetes job

--- pbench-controller/run.sh
+++ pbench-controller/run.sh
@@ -8,39 +8,13 @@ cp /root/.ssh/id_rsa /opt/pbench-agent/id_rsa
 sed -i "/^pbench_results_redirector/c pbench_results_redirector = ${pbench_server}" /opt/pbench-agent/config/pbench-agent.cfg
 sed -i "/^pbench_web_server/c pbench_web_server = ${pbench_server}"  /opt/pbench-agent/config/pbench-agent.cfg
 
-# Clear tools
-pbench-clear-tools
+# Setup tooling
+if [[ $JOB == "TOOLING" ]]; then
+	# Set pbench-server in the config
+	sed -i "/^pbench_results_redirector/c pbench_results_redirector = ${PBENCH_SERVER}" /opt/pbench-agent/config/pbench-agent.cfg
+	sed -i "/^pbench_web_server/c pbench_web_server = ${PBENCH_SERVER}"  /opt/pbench-agent/config/pbench-agent.cfg
 
-# Register tools
-ansible-playbook -i /root/inventory /root/pbench/contrib/ansible/openshift/pbench_register.yml
-if [[ "$?" == 0 ]]; then
-        echo -e "-----------------------------------------------------------\n\n\n"
-        echo -e "		PBENCH TOOLS REGISTERED SUCCESSFULLY		\n\n\n"
-        echo "-----------------------------------------------------------"
-else
-        echo -e "-----------------------------------------------------------\n\n\n"
-        echo -e "		PBENCH REGISTRATION FAILED		\n\n\n"
-        echo "-----------------------------------------------------------"
-fi
-
-if [[ "${clear_results}" == "true" ]] || [[ "${clear_results}" == "True" ]]; then
-       pbench-clear-results
-fi       
-
-# Run the benchmark
-${benchmark}
-benchmark_status=$?
-
-if [[ "${move_results}" == "true" ]] || [[ "${move_results}" == "True" ]]; then
-        pbench-move-results
-fi
-
-if [[ "$benchmark_status" == 0 ]]; then
-	echo -e "-----------------------------------------------------------\n\n\n"
-	echo -e "		OCP SCALE TEST COMPLETED		\n\n\n"
-	echo "-----------------------------------------------------------"
-else
-	echo -e "-----------------------------------------------------------\n\n\n"
-	echo -e "		OCP SCALE TEST FAILED		\n\n\n"
-	echo "-----------------------------------------------------------"
+	cd /root/scale-cd-jobs/build-scripts
+	./setup_tooling.sh ${TOOLING_INVENTORY} "${OPENSHIFT_INVENTORY}" ${CONTAINERIZED} ${REGISTER_ALL_NODES}
+	cd /root/pbench/contrib/ansible/openshift
 fi
